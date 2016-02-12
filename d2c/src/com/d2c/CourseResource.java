ac4f84455ca3a34005f30ba37d615ac3914f2225
sy6746 <sy6746@mun.ca>
2016-02-12 10:49:19 -0330

    Initial set-up of Jersey and an example resource

---
+++ d2c/src/com/d2c/CourseResource.java
@@ -0,0 +1,51 @@
+package com.d2c;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/course")
+public class CourseResource {
+	
+	@GET
+	@Path("/{course_id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getCourseInfo(@PathParam("course_id") String courseID){
+		//Some sql shit to get my object
+		
+		//check that object exists
+		boolean check = true;
+		//if it exists then save it to a java object and return through response
+		if(check){
+			TransferableCourse c = new TransferableCourse();
+			return Response.ok()
+					.entity(c)
+					.build();
+		} else { // else return a not found
+			return Response.noContent().build();
+		}
+	}
+
+	@POST
+	@Path("/{course_id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createOrUpdateCourse(@PathParam("course_id") String courseID, 
+			TransferableCourse course){
+		//TODO make this post the course info to the DB
+		
+		try {
+			return Response.created(new URI("/"+courseID))
+					.build();
+		} catch (URISyntaxException e) {
+			return Response.serverError().build();
+		}
+	}
+}
---
+++ d2c/src/com/d2c/TransferableCourse.java
@@ -0,0 +1,12 @@
+package com.d2c;
+
+import java.util.List;
+
+public class TransferableCourse {
+	public String courseNumber;
+	public List<TransferableStudent> classList;
+	public List<TransferableAssignment> assignments;
+	public String subject;
+	public String courseName;
+	public List<TransferableTeachingAssistant> teachingAssistants;
+}
