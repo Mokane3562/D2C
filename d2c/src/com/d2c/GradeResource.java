6506f192fe9cd9aa1fffd5ce312eabdf2436acbb
Michelle Croke <mlc258@mun.ca>
2016-02-15 10:37:47 -0330

    GradeResource.java and TransferableGrade.java and AssignmentResource.java

--- d2c/src/com/d2c/GradeResource.java
+++ d2c/src/com/d2c/GradeResource.java
@@ -12,23 +12,24 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/assignment")
-public class AssignmentResource {
+@Path("/grade")
+public class GradeResource {
 	
 	@GET
-	@Path("/{course_id}/{assignment}")
+	@Path("/{course_id}/{student_user_name}/{assignment}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCourseInfo(@PathParam("course_id") String courseID, 
-			@PathParam("assignment") String assignment){
+			@PathParam("student_user_name") String studentUserName, 
+			@PathParam("assignment")String assignment){
 		//Some sql shit to get my object
 		
 		//check that object exists
 		boolean check = true;
 		//if it exists then save it to a java object and return through response
 		if(check){
-			TransferableAssignment a = new TransferableAssignment();
+			TransferableGrade g = new TransferableGrade();
 			return Response.ok()
-					.entity(a)
+					.entity(g)
 					.build();
 		} else { // else return a not found
 			return Response.noContent().build();
@@ -36,17 +37,21 @@ public class AssignmentResource {
 	}
 
 	@POST
-	@Path("/{course_id}/{assignment}")
+	@Path("/{course_id}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response createOrUpdateAssignment(@PathParam("course_id") String courseID, 
-			@PathParam("assignment") String assignment){
+	public Response createOrUpdateCourse(@PathParam("course_id") String courseID, 
+			@PathParam("student_user_name") String studentUserName, 
+			@PathParam("assignment")String assignment, TransferableGrade grade){
 		//TODO make this post the course info to the DB
 		
 		try {
-			return Response.created(new URI("/"+courseID+"/"+assignment))
+			return Response.created(new URI("/"+courseID+"/"+studentUserName +"/"+assignment))
 					.build();
 		} catch (URISyntaxException e) {
 			return Response.serverError().build();
 		}
 	}
+	
+	//POST /grade/{course_id}/{assignment}
+
 }
