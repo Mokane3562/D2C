6b8eb8c16a62d26e86eae1895411bf988b769b05
sy6746 <sy6746@mun.ca>
2016-02-15 08:30:38 -0330

    Added the main app and an empty main controller.

---
+++ d2c/WebContent/WEB-INF/controllers/main_controller.js
@@ -0,0 +1,8 @@
+/**
+ * Every service you use will need to be injected.
+ * To do so, add the name as a string to the start of the array,
+ * and add the name as a variable to the function.
+ */
+app.controller('main_controller',['$scope' ,function($scope){
+	
+}]);
