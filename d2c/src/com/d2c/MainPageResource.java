c149f1a114cd9c92ebebc19b3f2f9075f6f3ee48
sy6746 <sy6746@mun.ca>
2016-02-17 09:02:32 -0330

    Removed unused imports

--- d2c/src/com/d2c/MainPageResource.java
+++ d2c/src/com/d2c/MainPageResource.java
@@ -1,11 +1,8 @@
 package com.d2c;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
