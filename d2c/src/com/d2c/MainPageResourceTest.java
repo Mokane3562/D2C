1d9dcd1311335ab715b586f6bb02a5917221bccd
sy6746 <sy6746@mun.ca>
2016-02-23 12:48:48 -0330

    Added MainPageResourceTest Unit Test Class

---
+++ d2c/src/com/d2c/MainPageResourceTest.java
@@ -0,0 +1,14 @@
+package com.d2c;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainPageResourceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
