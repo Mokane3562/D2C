0a51aca516a98b6e8eed51cfa5343a705edd6de0
Michelle Croke <mlc258@mun.ca>
2016-02-26 16:43:37 -0330

    added angular go-click in example_service.js

--- d2c/WebContent/WEB-INF/services/example_service.js
+++ d2c/WebContent/WEB-INF/services/example_service.js
@@ -1,15 +1,23 @@
-/**
- * 
- */
-app.factory('example_service',[function(){
-	return function(){
-		return [
-		    {
-				"name": "Grades"
-			},
-			{
-				"name": "Submission"
-			}
-		];
-	};
-}]);
+var app = angular.module('d2c_main', []);
+
+    app.directive( 'goClick', function ( $location ) {
+      return function ( scope, element, attrs ) {
+        var path;
+        
+        attrs.$observe( 'goClick', function (val) {
+          path = val;
+        });
+        
+        element.bind( 'click', function () {
+          scope.$apply( function () {
+            $location.path( path );
+          });
+        });
+      };
+    });
+
+app.controller('main_controller', function($scope, $location) {
+  $scope.$watch( function () { return $location.path(); }, function (path) {
+    $scope.path = path;
+  });
+});
