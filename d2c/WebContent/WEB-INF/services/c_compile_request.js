1d9ed02b201036b86e1204bcba6674c120a22159
sy6746 <sy6746@mun.ca>
2016-02-26 15:16:07 -0330

    added the service to call the c code compile request

--- d2c/WebContent/WEB-INF/services/c_compile_request.js
+++ d2c/WebContent/WEB-INF/services/c_compile_request.js
@@ -5,12 +5,20 @@ app.factory('c_compile_request', '$q', '$http',[function($q, $http){
 	return function(code, user, path){
 		var deferred = $q.defer();
 		$http({
-			method: 'GET',
-			url: 'localhost.com:8080/'
+			method: 'POST',
+			url: 'localhost.com:8080/'+user+'/'+path,
+			headers: {
+				'Content-Type': text/plain,
+				'access-control-allow-origin': '*'
+			},
+			data: {
+				content: code
+			}
 		}).then(function success(response){
-			
-		}, function error(){
-			
+			deferred.resolve(response);
+		}, function error(errors){
+			deferred.reject(errors);
 		});
+		return deferred.promise;
 	};
 }]);
