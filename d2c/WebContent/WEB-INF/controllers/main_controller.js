6032fcf14b4847657c6fc45952542dbea2dac0e8
sy6746 <sy6746@mun.ca>
2016-02-28 10:16:58 -0330

    Example service changed back to blank service
    goClick directive in new directive file 'goClick.js' and functionality
    compartmentalized into proper files.

--- d2c/WebContent/WEB-INF/controllers/main_controller.js
+++ d2c/WebContent/WEB-INF/controllers/main_controller.js
@@ -3,11 +3,14 @@
  * To do so, add the name as a string to the start of the array,
  * and add the name as a variable to the function.
  */
-app.controller('main_controller',['$scope', 'example_service', function($scope, example_service){
+app.controller('main_controller',['$scope', '$location', 'example_service', function($scope, $location, example_service){
 	$scope.course = example_service()[0];
 	$scope.submission = example_service()[1];
 	$scope.compile_c = c_compile_request();
 	$scope.run_c = c_run_request();
 	$scope.compile_java = java_compile_request();
 	$scope.run_java = java_run_request();
+	$scope.$watch( function () { return $location.path(); }, function (path) {
+	    $scope.path = path;
+	});
 }]);
--- d2c/WebContent/WEB-INF/services/example_service.js
+++ d2c/WebContent/WEB-INF/services/example_service.js
@@ -1,23 +1,5 @@
-var app = angular.module('d2c_main', []);
-
-    app.directive( 'goClick', function ( $location ) {
-      return function ( scope, element, attrs ) {
-        var path;
-        
-        attrs.$observe( 'goClick', function (val) {
-          path = val;
-        });
-        
-        element.bind( 'click', function () {
-          scope.$apply( function () {
-            $location.path( path );
-          });
-        });
-      };
-    });
-
-app.controller('main_controller', function($scope, $location) {
-  $scope.$watch( function () { return $location.path(); }, function (path) {
-    $scope.path = path;
-  });
+app.factory('example_service',function(){
+	return function() {
+		
+	}
 });
