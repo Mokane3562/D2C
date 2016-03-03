47ca2b1efecfedca3ff38a1527b305c06d28e7e5
Michelle Croke <mlc258@mun.ca>
2016-03-03 18:25:27 -0330

    Adding j_compile_request & changes to main_controller and J compile code button

---
+++ d2c/WebContent/WEB-INF/services/j_compile_request.js
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+app.factory('j_compile_request',[ '$q', '$http', function($q, $http){
+	return function(code, user, path){
+		var deferred = $q.defer();
+		console.log('/d2c/code/'+user+'/'+path.split("/").join("_")+'/j');
+		$http({
+			method: 'POST',
+			url: '/d2c/code/'+user+'/'+path.split("/").join("_")+'/j',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8;',
+				'accept': 'text/plain',
+				'access-control-allow-origin': '*'
+			},
+			data: code
+		}).then(function success(response){
+			console.log("success up to http");
+			deferred.resolve(response);
+		}, function error(errors){
+			console.log("failure at http");
+			deferred.reject(errors);
+		});
+		console.log("finished sending request");
+		return deferred.promise;
+	};
+}]);
