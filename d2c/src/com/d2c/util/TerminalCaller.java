8c6ccfcc47ed4e773da20967662cba448ca19fae
Mike Singleton <mikesingleton.24@gmail.com>
2016-03-04 13:03:05 -0330

    Added some comments to TerminalCaller

--- d2c/src/com/d2c/util/TerminalCaller.java
+++ d2c/src/com/d2c/util/TerminalCaller.java
@@ -9,23 +9,40 @@ import java.util.List;
 
 public class TerminalCaller {
 
+	/**
+	 * Saves a file to a specific path on the file system and creates any required folder hierarchy.
+	 * 
+	 * @param user	The owner of the file
+	 * @param path	The path to the file on the file system
+	 * @param name	The name of the file
+	 * @param contents	The contents of the file to be saved
+	 * @throws IOException	If an I/O error occurs
+	 */
 	public static void saveFile(String user, String path, String name, String contents) throws IOException {
 		// make temporary file
 		File dir = new File("/tmp/" + user + path);
 		File file = new File("/tmp/" + user + path + "/" + name);
 		dir.mkdirs();
 		file.createNewFile();
+		
 		// write code contents to temporary file
 		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
 		bw.write(contents);
 		bw.close();
 	}
 
+	/**
+	 * @param user
+	 * @param path
+	 */
 	public static void clearTempUserFiles(String user, String path) {
 		File file = new File("/tmp/" + user);
 		delete(file);
 	}
 
+	/**
+	 * @param file
+	 */
 	private static void delete(File file) {
 		if (file.isDirectory()) {
 			for (File subFile : file.listFiles()) {
@@ -35,6 +52,16 @@ public class TerminalCaller {
 		file.delete();
 	}
 
+	/**
+	 * Comiles C code the 'gcc' terminal command 
+	 * 
+	 * @param user
+	 * @param path
+	 * @param args
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static IOPipe gcc(String user, String path, List<String> args) throws IOException, InterruptedException {
 		ArrayList<String> command = new ArrayList<>();
 		command.add("gcc");
@@ -42,6 +69,14 @@ public class TerminalCaller {
 		return call(user, path, command);
 	}
 
+	/**
+	 * @param user
+	 * @param path
+	 * @param args
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static IOPipe aout(String user, String path, List<String> args) throws IOException, InterruptedException {
 		ArrayList<String> command = new ArrayList<>();
 		command.add("./a.out");
@@ -49,6 +84,14 @@ public class TerminalCaller {
 		return call(user, path, command);
 	}
 
+	/**
+	 * @param user
+	 * @param path
+	 * @param args
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static IOPipe javac(String user, String path, List<String> args) throws IOException, InterruptedException {
 		ArrayList<String> command = new ArrayList<>();
 		command.add("javac");
@@ -56,6 +99,14 @@ public class TerminalCaller {
 		return call(user, path, command);
 	}
 
+	/**
+	 * @param user
+	 * @param path
+	 * @param args
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static IOPipe java(String user, String path, List<String> args) throws IOException, InterruptedException {
 		ArrayList<String> command = new ArrayList<>();
 		command.add("java");
@@ -63,6 +114,14 @@ public class TerminalCaller {
 		return call(user, path, command);
 	}
 
+	/**
+	 * @param user
+	 * @param path
+	 * @param args
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public static IOPipe ls(String user, String path, List<String> args) throws IOException, InterruptedException {
 		ArrayList<String> command = new ArrayList<>();
 		command.add("ls");
@@ -70,6 +129,14 @@ public class TerminalCaller {
 		return call(user, path, command);
 	}
 
+	/**
+	 * @param user
+	 * @param path
+	 * @param command
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	private static IOPipe call(String user, String path, List<String> command) throws IOException, InterruptedException {
 		ProcessBuilder processBuilder = new ProcessBuilder(command).redirectErrorStream(true).directory(new File("/tmp/"
 				+ user + path));
