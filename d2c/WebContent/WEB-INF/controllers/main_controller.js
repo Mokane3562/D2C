4e362c8d1f3458bb802d6189ddecfc592872f2c8
rmp255 <rmp255@mun.ca>
2016-03-22 16:11:07 -0230

    Main page changes: nav bar, workspace and functionality

--- d2c/WebContent/WEB-INF/controllers/main_controller.js
+++ d2c/WebContent/WEB-INF/controllers/main_controller.js
@@ -20,7 +20,7 @@ app.controller('main_controller',['$scope', '$location', 'example_service', 'c_c
 	$scope.password = "";
 
 	//Setting view invisible
-	view["login"] = true;
+	view["login"] = false;
 	view["signup"] = false;
 	view["courseInfo"] = false;
 	view["assignments"] = false;
